d84d509cdb7bb8db9703881dda8bfc73973f2335
David Dasenbrook <david.dasenbrook@tngtech.com>
2018-06-22 19:08:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# API to Markdown
+A tool for converting data obtained from an API to a set of markdown files for use in a static site generator.
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+if (process.argv.length < 3) {
+    console.log('Please specify the URL and output path.');
+    process.exit();
+}
+
+console.log('Hello.');
