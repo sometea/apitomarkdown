9fe885c85eb077b46bb2c436d863a23702458acf
David Dasenbrook <david.dasenbrook@gmail.com>
2018-06-24 18:49:06 +0200

    create folder if it doesn't already exit

--- src/ResourceManager.ts
+++ src/ResourceManager.ts
@@ -6,7 +6,8 @@ export class ResourceManager {
     constructor(private listResource: ListResource, private outputPath: string) { }
 
     private toCamelCase(str: string ) {
-        return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr, idx) => idx === 0 ? ltr.toLowerCase() : ltr.toUpperCase()).replace(/\s+/g, '');
+        const camelCase = str.replace(/(?:^\w|[A-Z]|\b\w)/g, (ltr, idx) => idx === 0 ? ltr.toLowerCase() : ltr.toUpperCase()).replace(/\s+/g, '');
+        return camelCase.replace(/[^a-zA-Z ]/g, '');
     }
 
     private writeFile(fileNameWithPath: string, content: string): Promise<void> {
@@ -22,11 +23,18 @@ export class ResourceManager {
         });
     }
 
+    private createFolder(folderName: string): Promise<void> {
+        return new Promise(resolve => {
+            fs.mkdir(folderName, err => resolve());
+        });
+    }
+
     public async writeMarkdownFiles() {
         const detailResources = await this.listResource.iterate();
         for (let detailResource of detailResources) {
             const accessedResource = await detailResource.accessResource();
             const fileName = this.toCamelCase(accessedResource.getTitle()) + '.md';
+            await this.createFolder(this.outputPath);
             await this.writeFile(path.join(this.outputPath, fileName), accessedResource.asMarkdown());
         }
     }
